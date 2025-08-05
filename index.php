394bf3b321cae46363b5d900377c48214cbe017f
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2025-08-05 07:14:33 +0100

    Start of Php course

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+    $ime = "Igor";
+    $godine = 37;
+    $visina = 194.2;
+    $programer = true;
+
+    echo "Moje ime je $ime i imam $godine godina, a visok sam $visina cm.";
+
+    $visina = 195.2;
+    $ime = "Luka";
+    $godine = 36;
+
+    echo "Moje ime je $ime i imam $godine godina, a visok sam $visina cm.";
+
+?>
---
+++ vezba.php
@@ -0,0 +1,12 @@
+<?php
+
+    $cena = 500;
+    $porez = 100;
+
+    $totalnaCena = $cena + $porez;
+
+    echo $totalnaCena;
+
+
+
+?>
