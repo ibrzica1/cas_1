c3930577638625ff2f78165a970361088d814823
ibrzica1 <168496865+ibrzica1@users.noreply.github.com>
2025-08-05 09:00:38 +0100

    Lesson 1

--- vezba.php
+++ vezba.php
@@ -1,12 +1,27 @@
 <?php
 
     $cena = 500;
-    $porez = 100;
+    $iznosPoreza = 0.22;
+    $porez = $cena * $iznosPoreza;
 
     $totalnaCena = $cena + $porez;
 
-    echo $totalnaCena;
+    echo "Cena bez poreza: $cena, iznos poreza: $porez (procenat: $iznosPoreza), totalna cena: $totalnaCena";
 
+    $proizvod = "Hleb";
 
+    echo $proizvod;
 
+    $proizvod .= "Sava";
+
+    echo $proizvod;
+
+    $cenaProizvoda = 50;
+    $cenaProizvoda += 5;
+
+    echo $cenaProizvoda;
+
+    $cenaProizvoda++;
+
+    echo $cenaProizvoda;
 ?>
