d481706bfea671d038223b097764d23484fb03dd
ibrzica1 <igorbrzica1@gmail.com>
2026-01-15 16:40:49 +0100

    Lesson 32 Bootstrap

---
+++ Bootstrap/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css\bootstrap.min.css">
+    <link rel="stylesheet" href="css\style.css">
+    <script src="js\bootstrap.min.js"></script>
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-row">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 justify-content-evenly">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    
+  </div>
+</nav>
+    
+<div class="bg-success">
+    <p class="text-info">Hello World</p>
+</div>
+
+<section class="container">
+
+    <article class="col-7 bg-success">
+        <h1>Naslov</h1>
+        <h5>Podnaslov</h5>
+        <button class="btn btn-outline-primary">Buy</button>
+    </article>
+
+    <article class="bg-warning">
+        <h1>Naslov</h1>
+        <h5>Podnaslov</h5>
+        <button class="btn btn-outline-primary">Buy</button>
+    </article>
+
+    <article>
+        <h1>Naslov</h1>
+        <h5>Podnaslov</h5>
+        <button class="btn btn-outline-primary">Buy</button>
+    </article>
+
+    <article>
+        <h1>Naslov</h1>
+        <h5>Podnaslov</h5>
+        <button class="btn btn-outline-primary">Buy</button>
+    </article>
+
+    <article>
+        <h1>Naslov</h1>
+        <h5>Podnaslov</h5>
+        <button class="btn btn-outline-primary">Buy</button>
+    </article>
+
+</section>
+
+<footer class="bg-dark p-4 text-white d-flex justify-content-center">
+    <p>Moj Sajt</p>
+</footer>
+
+</body>
+
+</html>
