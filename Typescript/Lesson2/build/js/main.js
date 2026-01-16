b7a1e96a21c8505cc71616828e169b145a096161
ibrzica1 <igorbrzica1@gmail.com>
2026-01-16 16:58:29 +0100

    TS Lesson 2 Data Types

---
+++ Typescript/Lesson2/build/js/main.js
@@ -0,0 +1,16 @@
+let myName;
+let meaningOfLife;
+let isLoading;
+let album;
+myName = 'John';
+meaningOfLife = 42;
+isLoading = true;
+album = 1984;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
+export {};
+//# sourceMappingURL=main.js.map
